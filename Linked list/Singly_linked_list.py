3a42a1b26bf418b8e9c29d49daf23684339be433
Mohankrish08 <mohankrishce@gmail.com>
2023-10-02 19:52:50 +0530

    Updated operations

--- Linked list/Singly_linked_list.py
+++ Linked list/Singly_linked_list.py
@@ -55,6 +55,13 @@ class LinkedList:
             self.head = new_node
         self.length += 1
 
+    # Deleting all the elements in the linkedlist
+
+    def delete_all(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
     # Traverse the linkedlist to print the elements
 
     def traverse(self):
@@ -62,7 +69,60 @@ class LinkedList:
         while current is not None:
             print(current.value)
             current = current.next
-            
+
+    # Poping the first element
+
+    def pop_first(self):
+        if self.head is None:
+            return None
+        popped_node = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head.next = self.head
+            popped_node.next = None
+        self.length -= 1
+        return popped_node
+
+    # Poping the last element
+
+    def pop(self):
+        if self.head is None:
+            return None
+        elif self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            while temp is not self.tail:
+                temp = temp.next
+            self.tail = temp
+            temp.next = None
+        self.length -= 1
+        return temp
+    
+    # Deleting the particular element
+
+    def delete_particular(self, index):
+        if (index < 0 or index >= self.length):
+            return None
+        if self.length == 1:
+            return self.pop_first()
+        elif index == -1:
+            return self.pop()
+        else:
+            temp = self.head
+            for _ in range(index-1):
+                temp = temp.next
+            empty = temp.next
+            temp.next = empty.next
+            empty.next = None
+        self.length -= 1
+        
+        
+
+
     # Reversing the linkedlist
 
     def reverse(self):
@@ -81,6 +141,9 @@ linkedlist.append(20)
 linkedlist.prepend(10)
 linkedlist.insert(1,30)
 linkedlist.insert(3,40)
+print(linkedlist.traverse())
+print('--------------------------------------------')
+linkedlist.delete_particular(1)
 print(linkedlist.traverse())
 print('--------------------------------------------')
 linkedlist.reverse()
